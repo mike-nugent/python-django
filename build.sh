e46ea50f20fe97a4b3dcd9bbbe1c51cbb513a592
Mike Nugent <mike_nugent@live.com>
2019-04-26 23:47:22 -0700

    changing port

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker kill $(docker ps -aq)
 docker rm $(docker ps -aq --filter name=monk-nginx)
 # docker system prune -a -f
 docker build -t monk-nginx .
-docker run --name monk-nginx -d -p 8080:80 monk-nginx
+docker run --name monk-nginx -d -p 80:80 monk-nginx
